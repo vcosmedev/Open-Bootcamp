67ccf6dde249ef50e7ee7d05fcc220c0ad1ece2b
Víctor Cosme <victorcosmema@gmail.com>
2023-03-02 20:36:44 -0600

    Add Java exercise 3

---
+++ Intro-Programación/exercise3.js
@@ -0,0 +1,47 @@
+// Java
+
+/*
+Primera parte:
+
+- Crear una función con tres parámetros que sean números que se suman entre sí.
+- Llamar a la función en el main y darle valores.
+*/
+
+// public static void int main(String[] args) {
+// 	int result = addition(a: 5, b: 5, c: 5);
+// 	Sytem.out.println(result);
+// }
+
+// public static int addition( a, int b, int c){
+// 	return a + b + c;
+// }
+
+/* Segunda parte:
+
+- Crear una clase coche.
+- Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+- Una función que incremente el número de puertas que tiene el coche.
+- Crear un objeto miCoche en el main y añadirle una puerta.
+- Mostrar el número de puertas que tiene el objeto.
+
+*/
+
+// public class Car {
+
+//     var doors = 3;
+
+//     public static void main(String[] args) {
+//         int result = 0;
+//         result = addition(doors: 3, number: 2);
+//         System.out.println(result);
+//     }
+
+//     public static int addition(int doors, int number) {
+//         return doors + number;
+//     }
+
+//     class MyCar {
+//         Doors= "5";
+//     }
+
+// }
