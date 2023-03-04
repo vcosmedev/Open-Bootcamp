1db0ac15d1e2ff203213a44a5ef1b4d28f2ba0ef
Víctor Cosme <victorcosmema@gmail.com>
2023-03-03 21:37:28 -0600

    Add Java exercise 4

---
+++ Intro-Programación/exercise4.js
@@ -0,0 +1,97 @@
+// Java
+
+/*
+
+En este ejercicio practicarás las estructuras de control, para ello deberás crear:
+
+1) Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
+
+2) Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+
+    2.1.) Incrementar el valor de la variable en uno cada vez que se ejecute.
+
+    2.2) Mostrarlo por pantalla cada vez que se ejecute.
+
+3) Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+
+4) Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, 
+   se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+
+5) Por último, para el Switch, deberás crear la variable estación, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estación 
+   se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.
+
+*/
+
+// First exercise
+// public class Main {
+
+// 	public static void main(String[] args){
+// 		String numeroIf = "2";
+
+// 		if (season == "0"){
+// 			System.out.println("¡Es cero!")
+// 		} else if (season > "0"){
+// 				System.out.println("¡Es un número positivo!")
+// 		} else {
+// 				System.out.println("¡¡Es un número negativo!!")
+// 		}
+// 	}
+// }
+
+// Second exercise
+// public class Main {
+//     public static void main(String[] args){
+//         int numeroWhile = 0;
+
+//         while (numeroWhilte < 3) {
+//             System.out.println(numeroWhile);
+//             numeroWhile = numeroWhile++;
+//         }
+//     }
+// }
+
+// Third exercise
+// public class Main {
+// 	public static void main(String[] args){
+// 		int numeroDoWhile = 2;
+
+// 		do {
+// 			System.out.println(numeroDoWhile);
+// 			numeroDoWhile = numeroDoWhile++;
+// 		} while (numeroDoWhile < 3);
+// 	}
+// }
+
+// Forth exercise
+// public class Main {
+// 	public static void main(String[] args){
+// 		for (int numeroFor = 0; numeroFor <= 3; numeroFor = numeroFor++;)
+// 			System.out.println(numeroFor);
+// 	}
+// }
+
+// Fith exercise
+// public class Main {
+
+//     public static void main(String[] args) {
+//         let season = "Spring";
+
+//         switch(season){
+//             case "Spring":
+//                 System.out.println("It's spring 🌸")
+//                 break;
+//             case "Summer":
+//                 System.out.println("It's summer 🌞")
+//                 break;
+//             case "Autumn":
+//                 System.out.println("It's autumn 🍂")
+//                 break;
+//             case "Winter":
+//                 System.out.println("It's winter 🥶")
+//                 break;
+//             default:
+//                 System.out.println("Are you sure that was a correct season? 🤪")
+//         }
+//     }
+// }
