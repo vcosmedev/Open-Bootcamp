0fd9732a66713ee853288c459db7a48f703cdc33
Víctor Cosme <victorcosmema@gmail.com>
2023-03-05 13:58:51 -0600

    Add Java exercise 9

---
+++ Intro-Programación/exercise9.js
@@ -0,0 +1,55 @@
+// JAVA
+
+// Crea una **clase Persona** con las siguientes variables:
+
+// - La edad
+// - El nombre
+// - El teléfono
+
+// Una vez creada la clase, crea una nueva **clase Cliente** que **herede** de **Persona**, esta nueva clase tendrá la variable **crédito** solo para esa clase.
+
+// Crea ahora un **objeto** de la **clase Cliente** que debe tener como propiedades **la edad, el teléfono, el nombre y el crédito**, tienes que darles valor y
+// mostrarlas por pantalla.
+
+// Una vez hecho esto, haz lo mismo con la **clase Trabajador** que **herede** de **Persona**, y con una variable **salario** que solo tenga la clase Trabajador.
+
+// public class Main{
+
+//     public static void main(String[] args)
+//         Cliente cliente = new Cliente();
+//         cliente.setEdad = "30";
+//         cliente.setTelefono = "123456789";
+//         cliente.setNombre = "Víctor";
+//         cliente.setCredito = "40.000";
+
+//         System.out.println(cliente.setEdad());
+//         System.out.println(cliente.setTelefono());
+//         System.out.println(cliente.setNombre());
+//         System.out.println(cliente.setCredito());
+
+//         Trabajador trabajador = new Trabajador();
+//         trabajador.setEdad = "30";
+//         trabajador.setNombre = "Víctor";
+//         trabajador.setTelefono = "123456789";
+//         trabajador.setSalario = "25.000";
+
+//         System.out.println(trabajador.setEdad());
+//         System.out.println(trabajador.setNombre());
+//         System.out.println(trabajador.setTelefono());
+//         System.out.println(trabajador.setSalario());
+
+// }
+
+// class Persona {
+//     int edad;
+//     String nombre;
+//     int telefono;
+// }
+
+// final class Cliente extends Persona {
+//     int credito;
+// }
+
+// final class Trabajador extends Persona {
+//     int salario;
+// }
