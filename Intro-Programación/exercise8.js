f0018fc949e9475d6d8631363f9cc2cc7d50b96a
Víctor Cosme <victorcosmema@gmail.com>
2023-03-04 22:03:23 -0600

    Add Java exercise 8

---
+++ Intro-Programación/exercise8.js
@@ -0,0 +1,66 @@
+// JAVA
+
+// Para practicar la encapsulación:
+
+// Crear clase Persona.
+
+// Crear variables las privadas edad, nombre y teléfono de la clase Persona.
+
+// Crear gets y sets de cada propiedad.
+
+// Crear un objeto persona en el main.
+
+// Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono, por último muéstralas por consola.
+
+// public class Main{
+//     public static void main(String[] args){
+
+//         Persona personaUno = new Persona();
+//         persona.setEdad("30");
+//         persona.setNombre("Víctor");
+//         persona.setTelefono("123456789");
+//         Persona personaDos = new Persona();
+//         persona.setEdad("32");
+//         persona.setNombre("Gorka");
+//         persona.setTelefono("987654321");
+
+//         System.out.println(personaUno.getEdad());
+//         System.out.println(personaUno.getNombre());
+//         System.out.println(personaUno.getTelefono());
+//         System.out.println(personaDos.getEdad());
+//         System.out.println(personaDos.getNombre());
+//         System.out.println(personaTres.getTelefono());
+
+//     }
+// }
+
+// class Persona {
+//     private String edad;
+//     private String nombre;
+//     private String teléfono;
+
+//     public void setNombre(String valor){
+//         this.nombre = valor;
+//     }
+
+//     public String getNombre(){
+//         this.nombre = nombre;
+//     }
+
+//     public void setEdad(String valor){
+//         this.edad = valor;
+//     }
+
+//     public String getEdad(){
+//         this.edad = edad;
+//     }
+
+//     public void setTelefono(String valor){
+//         this.telefono = valor;
+//     }
+
+//     public String getTelefono(){
+//         this.telefono = telefono;
+//     }
+
+// }
