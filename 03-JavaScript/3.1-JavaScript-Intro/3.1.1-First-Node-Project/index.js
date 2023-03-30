627e62991d9b43beabbfa66d7755a29a1683f330
Víctor Cosme <victorcosmema@gmail.com>
2023-03-29 18:36:44 -0600

    Add First Node Project example

---
+++ 03-JavaScript/3.1-JavaScript-Intro/3.1.1-First-Node-Project/index.js
@@ -0,0 +1,13 @@
+// Welcome message
+console.log("Hello World from index.js!");
+
+/* 
+
+Esta es la parte intermedia del proyecto
+En esta parte realizamos los chequeos necesearios
+Finalmente, esta es la última línea antes del mensaje de despedida
+
+*/
+
+// Last message
+console.log("Bye!");
