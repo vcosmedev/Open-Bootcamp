1adae616a03b964736e96e1ca40ed52b1b21e997
Víctor Cosme <victorcosmema@gmail.com>
2023-03-30 00:36:57 -0600

    Add Types in JS

---
+++ 03-JavaScript/3.2-TypedJS-PrimitiveTypes/3.2.1-Types/index.js
@@ -0,0 +1,24 @@
+// Tipos primitivos
+
+// Number
+4;
+
+// String
+("Hello World!");
+("Hello World!");
+`Hello World`;
+
+// Boolean
+true;
+false;
+
+// Null & Undefined
+null; // -> Espacio vacío en memoria
+undefined; // -> Valor vacío, no especificado en memoria
+
+// Null, Undefined, false, 0
+if (null) {
+  console.log("OK");
+} else {
+  console.log("NOT OK");
+}
