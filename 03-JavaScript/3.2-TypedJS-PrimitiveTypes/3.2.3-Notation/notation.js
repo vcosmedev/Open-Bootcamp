239054f229d804274ca446f7042926a285061b09
Víctor Cosme <victorcosmema@gmail.com>
2023-03-30 20:24:05 -0600

    Add Notation in JS and delete untitled file from Variables

---
+++ 03-JavaScript/3.2-TypedJS-PrimitiveTypes/3.2.3-Notation/notation.js
@@ -0,0 +1,33 @@
+// NOTATION
+
+//   ;   -> Delimita el final de una línea
+const b = 4;
+b + 4;
+
+//   .   -> Se utiliza en los objetos para acceder a los atributos
+// mobile.lenght
+
+// Square brackets  []  -> Se utilizan en listas / arreglos / arrays
+const arr = [1, 2, 3, 4];
+console.log(arr[2]); // EO: 3
+
+// Parenthesis  ()  -> Se utilizan para funciones
+function addition(a, b) {
+  // Here the function is written
+}
+
+// Curly braces  {}  -> Se utiilizan para objetos, funciones y estructuras de control
+// const obj = {
+//     ...
+// }
+
+const mobile = {
+  width: 5,
+  height: 10,
+};
+
+if (true) {
+  // Everything between these curly braces
+
+  const constant2 = "hola";
+}
