dc70bed022c2019f3c785862f65cfbb5a7f9c81c
Víctor Cosme <victorcosmema@gmail.com>
2023-03-30 21:16:05 -0600

    Add Exercise List-Objects-Dates

---
+++ 03-JavaScript/3.2-TypedJS-PrimitiveTypes/Final Exercise/finalExercise.js
@@ -0,0 +1,9 @@
+const book = {
+  title: "Educated",
+  author: "Tara Westover",
+  date: "Jan 2017",
+  url: "shorturl.at/dhpEF",
+};
+
+const list = ["Victor", 32, true, "Nov 11", book];
+console.log(list);
