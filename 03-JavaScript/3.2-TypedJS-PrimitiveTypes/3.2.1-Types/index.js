c52d585a11b2f539625355203887305afc39cd7e
Víctor Cosme <victorcosmema@gmail.com>
2023-03-30 18:44:21 -0600

    Add Types in JS

--- 03-JavaScript/3.2-TypedJS-PrimitiveTypes/3.2.1-Types/index.js
+++ 03-JavaScript/3.2-TypedJS-PrimitiveTypes/3.2.1-Types/index.js
@@ -16,9 +16,39 @@ false;
 null; // -> Espacio vacío en memoria
 undefined; // -> Valor vacío, no especificado en memoria
 
-// Null, Undefined, false, 0
+// Null, Undefined, false, 0 --> TODAS SON FALSY
 if (null) {
   console.log("OK");
 } else {
   console.log("NOT OK");
 }
+
+if (undefined) {
+  console.log("OK");
+} else {
+  console.log("NOT OK");
+}
+
+if (false) {
+  console.log("OK");
+} else {
+  console.log("NOT OK");
+}
+
+if (0) {
+  console.log("OK");
+} else {
+  console.log("NOT OK");
+}
+
+if (true) {
+  console.log("OK");
+} else {
+  console.log("NOT OK");
+}
+
+//   Aunque todas son FALSY aunque no son el mismo tipo
+console.log(undefined === null);
+console.log(null === false);
+console.log(false === 0);
+console.log(0 === undefined);
