da2fc454166dafd46d130a2aa66234401932b120
Víctor Cosme <victorcosmema@gmail.com>
2023-04-06 20:12:45 -0600

    Add Console Use in JS

---
+++ 03-JavaScript/3.7-Sets-and-Objects/3.7.4-Console-Use/index.js
@@ -0,0 +1,14 @@
+// REQUERIR INPUT/INFORMACIÓN AL USUARIO
+// about:blank > console
+
+// función 'propmt'
+// prompt("How old are you?");
+// 30
+
+// Para guardar el input que ha ingresado el usuario en una variable:
+// const age = prompt("How old are you?");
+// age = '30'; -> String
+
+// Si se requiere edad en formato number:
+// const num_age = Number(age);
+// num_age = 30;
