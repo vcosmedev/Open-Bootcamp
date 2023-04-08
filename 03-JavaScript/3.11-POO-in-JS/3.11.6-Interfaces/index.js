3b03a4b8d45292427c6f114190ba0d8d396411ca
Víctor Cosme <victorcosmema@gmail.com>
2023-04-08 16:28:19 -0600

    Add brief Interfaces intro in JS

---
+++ 03-JavaScript/3.11-POO-in-JS/3.11.6-Interfaces/index.js
@@ -0,0 +1,9 @@
+// Interfaces
+// A priori en JS no existen interfaces ya que es un lenguaje interpretado, es decir, se va interpretando 'por el camino' -> duckt typing
+// Duck typing -> No hace falta que todo esté definido para poder ser utilizado o declarado
+// Duck typing: Si hace 'cuack', anda y vuela como un pato, para JS es un pato ;))))
+
+// En otros lenguajes de programación como C# se basan en la interpretación 'on the flight'
+
+// Sin embargo, para TypeScript sí existe y se pueden utilizar las Interfaces
+// Curso Introducción TypeScritp - OpenBootcamp
