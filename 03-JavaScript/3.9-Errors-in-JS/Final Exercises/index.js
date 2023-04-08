d0f2f96aef8d2a0ba09cecc7f3592347b3d7a05b
Víctor Cosme <victorcosmema@gmail.com>
2023-04-07 19:31:10 -0600

    Add Errors Final Exercise

---
+++ 03-JavaScript/3.9-Errors-in-JS/Final Exercises/index.js
@@ -0,0 +1,23 @@
+const myFunction = (value) => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  }
+  // return false:
+  throw new Error("El valor debe ser de tipo 'string'");
+};
+
+const str = "sevilla";
+//   const str2 = 3;
+
+try {
+  console.log("Está ejecutándose de forma correcta :)");
+  const upperCase = myFunction(str); // ojo, 'numbers'
+  console.log(upperCase);
+} catch (e) {
+  console.error(`El valor de 'e' es: ${e}`);
+  console.error("ERROR!");
+} finally {
+  console.log("Esto se ejecuta se produzca o no un error ;)");
+}
+
+const logger = require("./logger");
