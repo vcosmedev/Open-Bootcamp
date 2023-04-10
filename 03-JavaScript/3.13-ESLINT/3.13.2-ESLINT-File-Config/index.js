fcdea42cb2865008f6445aeb20682bff73a2eed7
Víctor Cosme <victorcosmema@gmail.com>
2023-04-09 22:58:09 -0600

    Add ESLint File Configuration

---
+++ 03-JavaScript/3.13-ESLINT/3.13.2-ESLINT-File-Config/index.js
@@ -0,0 +1,10 @@
+// ESLINT
+// Rules -> https://eslint.org/docs/latest/rules/
+// Install ESLINT extension
+
+const name = "Víctor";
+
+let name2 = "Ander";
+
+// ERROR LENS EXTENSION
+// A la altura de la línea de código te indica el error para corregirlo de forma rápdia
