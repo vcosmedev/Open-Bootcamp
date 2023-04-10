ba90f96fe76ecea647f834508e82a759739564d8
Víctor Cosme <victorcosmema@gmail.com>
2023-04-09 23:31:11 -0600

    Add Custom ESLINT Config, part I

---
+++ 03-JavaScript/3.13-ESLINT/3.13.3-ESLINT-Custom-Config/index.js
@@ -0,0 +1,3 @@
+// npm init -y
+// First option: npm install eslint --save-dev
+// Second option: npm i -D eslint -> 'D' viene de Developer, son dependencias de desarrollo, solo estarán en el proyecto en el que se está trabajando
