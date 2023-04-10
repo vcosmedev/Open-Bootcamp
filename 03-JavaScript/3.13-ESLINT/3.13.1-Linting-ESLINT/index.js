71472b27c144f7565b141755436f2ebb837e2684
Víctor Cosme <victorcosmema@gmail.com>
2023-04-09 22:39:18 -0600

    Add ESLLINT Intro in JS

---
+++ 03-JavaScript/3.13-ESLINT/3.13.1-Linting-ESLINT/index.js
@@ -0,0 +1,7 @@
+// Linting + ESLINT
+// Linting -> Metodología mantener código homogéneo y evitar errores
+
+// Posibles inconsistencias: mezclar entrecomillados, puntos y comas en ocasiones sí y en otras no, prácticas con todos los console.logs, identación, etc.
+
+// ESLINT -> EcmaScript + Lint ('Lintear')
+// Rules -> https://eslint.org/docs/latest/rules/
