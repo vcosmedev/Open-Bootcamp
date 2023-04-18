eb260a2d1819dc3a3e75f2c45c2eab758b68c768
Víctor Cosme <victorcosmema@gmail.com>
2023-04-17 20:16:18 -0600

    Add HMTL Intro with JS

---
+++ 03-JavaScript/3.14-Events-Mngmnt/3.14.1-HMTL-Intro-With-JS/animation.js
@@ -0,0 +1,5 @@
+new TypeIt("#myElement", {
+  strings: ["This is a customized text ;)"],
+}).go();
+
+// https://www.typeitjs.com/docs/vanilla/
---
+++ 03-JavaScript/3.14-Events-Mngmnt/3.14.1-HMTL-Intro-With-JS/index.js
@@ -0,0 +1,12 @@
+// npm install --global http-server -> Dependencia que crea un servidor dentro de nuestro PC para poder acceder y visualizar nuestro código HMTL desde el navegador
+// npm init -y
+
+const a = 4;
+const b = 8;
+
+console.log(a * b);
+console.log("Hola");
+
+// document.getElementById("paragraph")
+const p = document.getElementById("paragraph");
+console.log(p);
